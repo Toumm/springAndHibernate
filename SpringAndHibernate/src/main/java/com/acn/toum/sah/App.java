e48706652e7717697c5a093d8eb88a50a4fb6449
unknown <fabien.thoumsin@MW7SODH35UMYNP.dir.svc.accenture.com>
2015-03-17 10:47:25 +0100

    Hibernate and Spring init and configuration + Employee class

--- SpringAndHibernate/src/main/java/com/acn/toum/sah/App.java
+++ SpringAndHibernate/src/main/java/com/acn/toum/sah/App.java
@@ -1,5 +1,15 @@
 package com.acn.toum.sah;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.acn.toum.model.Employee;
+
 /**
  * Hello world!
  *
@@ -8,6 +18,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	/*
+    	 * Hibernate configuration
+    	 */
+		Configuration configuration = new Configuration();
+		configuration.configure();
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+		SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		Session session = sessionFactory.openSession();
+		
+		/*
+		 * Spring configuration
+		 */
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+		Employee e = ctx.getBean("employee", Employee.class);
+		System.out.println( "Hello World!" );
     }
 }
