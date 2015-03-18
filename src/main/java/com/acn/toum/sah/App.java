ef8ae31bf0e13e2f6b8092c8fd2bd787b457b29e
thoumsinf@gmail.com <thoumsinf@gmail.com>
2015-03-18 09:17:51 +0100

    remove useless subfolder part 2

---
+++ src/main/java/com/acn/toum/sah/App.java
@@ -0,0 +1,55 @@
+package com.acn.toum.sah;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.acn.toum.model.Employee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	/*
+    	 * Hibernate configuration
+    	 */
+		Configuration configuration = new Configuration();
+		configuration.configure();
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+		SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		Session session = sessionFactory.openSession();
+		
+		/*
+		 * Spring configuration
+		 */
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+		
+		session.beginTransaction();
+		Employee e = ctx.getBean("employee", Employee.class);
+		e.setLastname("Thoumsin");
+		e.setFirstname("Fabien");
+		try{
+			session.persist(e);
+			session.save(e);
+			session.getTransaction().commit();
+			System.out.println( "Employee "+e.getFirstname()+" "+ e.getLastname()+" added!" );
+		}
+		catch(Exception ex){
+			System.out.println("An error occured...");
+		}
+		session.close();
+		
+    }
+    
+    public static String jean(){
+    	return "Salut";
+    }
+}
